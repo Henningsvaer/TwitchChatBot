d1691942ddfae40a0dafbff357f64fb7e67b9dcc
Anton Nikolaev <anton.nikolaev.kazan@gmail.com>
2024-06-30 20:42:50 +0300

    Base bot realization

--- ChatBot/Program.cs
+++ ChatBot/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using ChatBot.Http.Bot.Client;
+using ChatBot.Interfaces.Client;
+using Microsoft.Extensions.Configuration;
+
+//https://dev.twitch.tv/docs/irc/get-started/
+//https://dev.twitch.tv/console/apps/4ez3nzdxi5e7s5hddfya8wjcrydg2h
+// TODO: Add settings from json 
+IConfiguration configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.Development.json", optional: true)
+    .Build();
+
+IBot bot = new Bot(configuration);
+Console.ReadLine();
